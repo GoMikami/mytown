5f05ace5283653ff489324a2c2832c80243144be
GoMikami <08g0531mkm@gmail.com>
2021-03-14 02:07:17 +0000

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,22 @@
 Rails.application.routes.draw do
-  get 'article_comments/new'
-  get 'article_comments/index'
-  get 'article_comments/show'
-  get 'article_comments/create'
-  get 'article_comments/edit'
-  get 'article_comments/update'
-  get 'article_comments/destroy'
-  get 'articles/new'
-  get 'articles/index'
-  get 'articles/show'
-  get 'articles/create'
-  get 'articles/edit'
-  get 'articles/update'
-  get 'articles/destroy'
-  get 'genres/create'
-  get 'genres/edit'
-  get 'genres/update'
-  get 'genres/index'
-  devise_for :admins
-  devise_for :contributors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admin, controllers: {
+    sessions:      'admins/sessions',
+  }
+  devise_for :contributors, controllers: {
+    registrations: 'contributors/registrations',
+    sessions:      'contributors/sessions'
+  }
+  
+  root "homes#top"
+  get "about" => "homes#about"
+
+  resources :contributors
+  
+  resources :articles
+  
+  resources :article_comments
+  
+  resources :genres
+  
 end
